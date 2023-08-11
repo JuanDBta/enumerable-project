59f08546fb78ac302a943d8c7051adb459795432
JuanDBta <simplebetsas@gmail.com>
2023-08-11 13:32:28 -0500

    Fix typo errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,17 +1,17 @@
 module MyEnumerable
   def all?
-    each { |element| return false unless yield(element) }
+    @list.all? { |element| return false unless yield(element) }
     true
   end
 
   def any?
-    each { |element| return true if yield(element) }
+    @list.any? { |element| return true if yield(element) }
     false
   end
 
   def filter
     filtered_list = []
-    each { |element| filtered_list << element if yield(element) }
+    @list.filter { |element| filtered_list << element if yield(element) }
     filtered_list.inspect
   end
 end
