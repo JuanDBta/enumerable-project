d3c710b1b1ba50ee95c9d6a1e6944bab3c5567c1
JuanDBta <simplebetsas@gmail.com>
2023-08-11 12:33:28 -0500

    Add any functionality

--- enumerable.rb
+++ enumerable.rb
@@ -1,13 +1,15 @@
 module MyEnumerable
   def all?
-    @list.all? { |element| return true if yield(element) }
+    @list.all? { |element| return false unless yield(element) }
+    true
   end
 
-  def any?(&)
-    @list.any?(&)
+  def any?
+    @list.any? { |element| return true if yield(element) }
+    false
   end
 
-  def filter(&)
-    @list.filter(&)
+  def filter
+    @list.filter
   end
 end
